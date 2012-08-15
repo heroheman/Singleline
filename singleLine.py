2ede46274df2a206db0f0cd579be22faa0706059
Jesse Robertson <jessrobe@gmail.com>
2012-08-15 21:46:00 +1000

    now expands selection to current rule if cursor is inside

--- singleLine.py
+++ singleLine.py
@@ -1,10 +1,33 @@
-import sublime, sublime_plugin, re
+import sublime
+import sublime_plugin
+import re
 
-# toggle a single-line or multi-line formatted css statement
+
+def expand_to_css_rule(view, cur_point):
+    print "in expander"
+    '''expand cursor inside css rule to whole css rule'''
+    css_rules = view.find_all('(^ *\.?\w+) ?\{([^{])*\}')
+    for css_rule in css_rules:
+        print css_rule.a, css_rule.b, view.substr(css_rule)
+        if css_rule.contains(cur_point):
+            return css_rule
+    # just return cur_point region if nothing found
+    return cur_point
+
+
 class ToggleSingleLineCssCommand(sublime_plugin.TextCommand):
-   def run(self,edit):
-      for region in reversed(self.view.sel()):
+    '''toggle a single-line or multi-line formatted css statement'''
+    def run(self, edit):
+        print "======= entered toggle function ========"
+        for region in reversed(self.view.sel()):
+            # quit early if not in css area
+            if self.view.score_selector(region.a, 'source.css') == 0:
+                return
+
+            if region.empty():
+                region = expand_to_css_rule(self.view, region)
             text = self.view.substr(region)
+            print '>>> css region ==>', text
 
             # check if the css statement needs to be expanded or collapsed
             if re.match('^.*\{.*}\s*$', text):
